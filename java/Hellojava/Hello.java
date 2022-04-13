fe638b880b19b83d79cba9b86d7d0d5c7b2ad10c
Deepakpaneer <deepakthirup@gmail.com>
2022-04-13 12:08:48 +0530

    add anew file'

---
+++ java/Hellojava/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args) {
+      int myInt = 23 ;
+      double myDouble = myInt; // Automatic casting: int to double
+  
+      System.out.println(myInt);      // Outputs 9
+      System.out.println(myDouble);   // Outputs 9.0
+    }
+  }
---
+++ java/Hellojava/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] argS) 
+    {
+    String name="Deepak";
+    int myNum = 10;
+     System.out.println(name +" "+ myNum);   
+    }
+}
